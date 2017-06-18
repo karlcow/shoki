027806294ffe978691556f7611ca7ca50f69c652
Karl Dubost <karl+github@la-grange.net>
2017-06-18 23:53:53 +0900

    Adds config file

---
+++ shoki_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+'''This file contains the config elements for shoki.'''
+
+# Parsing format
+# The characters used for marking up the title of a discussion
+TOPIC_HEADER = "#"
+# string to stop parsing
+END = '===========AGENDA ITEMS ABOVE THIS LINE==========='
