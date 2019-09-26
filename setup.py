f5b6e1269dec8e9cda60164625ffe91c5729a4d8
Karl Dubost <karl+github@la-grange.net>
2019-09-26 15:23:54 +0900

    Issue #24 - Bumps version to 2.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name='shoki',
-    version='2.0.0',
+    version='2.0.2',
     description='Generate minutes from a text file',
     long_description=README,
     long_description_content_type='text/markdown',
